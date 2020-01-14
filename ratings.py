d21849e6de9db9993753c0e2b5b2dc4a6593c6d2
Your Name <you@example.com>
2020-01-13 17:30:17 -0800

    working code for further study in ratings.py

--- ratings.py
+++ ratings.py
@@ -13,6 +13,9 @@ def restaurant_scorer(filename):
             restaurant, rating = line.rstrip().split(':')
             scores[restaurant] = rating
 
+    new_restaurant, new_rating = gets_review()
+    scores[new_restaurant] = new_rating
+
     # pull the items from the dictionary
     # sort the items in the dictionary
     alpha_sorted_ratings = sorted(scores.items())
@@ -20,6 +23,29 @@ def restaurant_scorer(filename):
     # print recommended statement
     for restaurant, rating in alpha_sorted_ratings:
         print(f"{restaurant} is rated at {rating}.")
+
+
+def gets_review():
+    '''Asks user for their own restaurant name and review.'''
+
+    restaurant_name = input("Please write a restaurant name: ")
+
+    while True:
+        restaurant_score = input("Please give the score for your restaurant (1-5): ")
+        if validate_score(restaurant_score):
+            break
+
+    return (restaurant_name, restaurant_score)
+
+
+def validate_score(score):
+    try:
+        score = int(score)
+    except ValueError:
+        print("The score must be an integer between 1 and 5.")
+        return False
+
+    return 1 <= score <= 5
 
 
 restaurant_scorer('scores.txt')
